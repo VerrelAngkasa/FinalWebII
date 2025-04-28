9c0d735db1a79d5c4f550d5824f32b760f569e62
Verrel <verrelangkasa20@gmail.com>
2025-04-29 00:50:29 +0700

    update backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://sja-user:sja123@cluster1.8oox96x.mongodb.net/db-sja', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected successfully"))
